65daa61a581e7850080fe81bb48429962f27fb7e
unknown <michaelshur28@gmail.com>
2021-08-19 23:39:40 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
